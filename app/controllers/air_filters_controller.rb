aba20b9b2b7dac2ee591bbc337bfb5523aad5f90
kevinkorte <kevinkorte15@gmail.com>
2015-04-01 20:17:21 +0000

    Added New, Create, and Update actions

--- app/controllers/air_filters_controller.rb
+++ app/controllers/air_filters_controller.rb
@@ -16,11 +16,29 @@ class AirFiltersController < ApplicationController
     end
     
     def create
+        @airfilter = AirFilter.new(airfilter_params)
+        if @airfilter.save
+            redirect_to @airfilter
+        else
+            render :new
+        end
     end
     
     def update
+      @airfilter = AirFilter.find(params[:id])
+      if @airfilter.update_attributes(airfilter_params)
+        flash[:success] = "Update Successful"
+        redirect_to @airfilter
+      else
+        render 'edit'
+      end
     end
 
     def destroy
     end
+    
+    private
+        def airfilter_params
+            params.require(:air_filter).permit(:brand, :part_number)
+        end
 end
